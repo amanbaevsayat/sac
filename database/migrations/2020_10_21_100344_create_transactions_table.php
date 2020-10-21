df68249931efb0191353c5469b8936147c09f3f0
Sayat Amanbayev <sayat.amanbayev@airastana.com>
2020-10-21 10:05:35 +0600

    init transaction model

---
+++ database/migrations/2020_10_21_100344_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('subscription_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('subscription_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
