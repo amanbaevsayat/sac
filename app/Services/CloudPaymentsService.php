47d2496f5f1111d15810a96650b9292119fed93e
Sayat Amanbayev <sayat.amanbayev@airastana.com>
2020-10-21 14:21:33 +0600

    add main methods for cloudpayments service

--- app/Services/CloudPaymentsService.php
+++ app/Services/CloudPaymentsService.php
@@ -32,15 +32,120 @@ class CloudPaymentsService
             ->retry(3, 5000);
     }
 
+    // Отмену оплаты можно выполнить через личный кабинет либо через вызов метода API.
+    // https://developers.cloudpayments.ru/#otmena-oplaty
+    public function cancelPayment(int $transactionId)
+    {
+        return $this->response($this->call("/payments/void", [
+            "TransactionId" => $transactionId
+        ]));
+    }
+
+    // Возврат денег можно выполнить через личный кабинет, либо через вызов метода API.
+    // https://developers.cloudpayments.ru/#vozvrat-deneg
+    public function refundPayment(int $transactionId, float $amount)
+    {
+        return $this->response($this->call("/payments/refund", [
+            "TransactionId" => $transactionId,
+            "Amount" => $amount,
+        ]));
+    }
+
+    // Метод получения детализации по транзакции.
+    // https://developers.cloudpayments.ru/#prosmotr-tranzaktsii
+    public function getTransaction(string $transactionId)
+    {
+        return $this->response($this->call("/payments/get", [
+            "TransactionId" => $transactionId
+        ]));
+    }
+
+    // Метод поиска платежа и проверки статуса
+    // https://developers.cloudpayments.ru/#proverka-statusa-platezha
+    public function findPayment(string $invoiceId)
+    {
+        return $this->response($this->call("/payments/find", [
+            "InvoiceId" => $invoiceId
+        ]));
+    }
+
+    // Метод выгрузки списка транзакций за день.
+    // https://developers.cloudpayments.ru/#vygruzka-spiska-tranzaktsiy
     public function getTransactions(string $date)
     {
-        return $this->response($this->http->post("{$this->baseURL}/payments/list", [
+        return $this->response($this->call("/payments/list", [
             "date" => $date,
             "timezone" => $this->timezone
         ]));
     }
 
-    public function response(Response $response)
+    // Метод создания подписки на рекуррентные платежи.
+    // https://developers.cloudpayments.ru/#sozdanie-podpiski-na-rekurrentnye-platezhi
+    public function createSubscriptions(array $request)
+    {
+        return $this->response($this->call("/subscriptions/create", [
+            "token" => $request["token"],
+            "accountId" => $request["accountId"],
+            "description" => $request["description"],
+            "email" => $request["email"],
+            "amount" => $request["amount"],
+            "currency" => $request["currency"],
+            "requireConfirmation" => $request["requireConfirmation"],
+            "startDate" => $request["startDate"],
+            "interval" => $request["interval"],
+            "period" => $request["period"],
+        ]));
+    }
+
+    // Метод получения информации о статусе подписки.
+    // https://developers.cloudpayments.ru/#zapros-informatsii-o-podpiske
+    public function getSubscription(string $subscriptionId)
+    {
+        return $this->response($this->call("/subscriptions/get", [
+            "Id" => $subscriptionId
+        ]));
+    }
+
+    // Метод получения списка подписок для определенного аккаунта.
+    // https://developers.cloudpayments.ru/#poisk-podpisok
+    public function findSubscriptions(string $accountId)
+    {
+        return $this->response($this->call("/subscriptions/find", [
+            "accountId" => $accountId
+        ]));
+    }
+
+    // Метод изменения ранее созданной подписки.
+    // https://developers.cloudpayments.ru/#izmenenie-podpiski-na-rekurrentnye-platezhi
+    public function updateSubscription(array $request)
+    {
+        return $this->response($this->call("/subscriptions/update", [
+            "Id" => $request["Id"],
+            "Description" => $request["Description"] ?? null,
+            "Amount" => $request["Amount"] ?? null,
+            "Currency" => $request["Currency"] ?? null,
+            "RequireConfirmation" => $request["RequireConfirmation"] ?? null,
+            "StartDate" => $request["StartDate"] ?? null,
+            "Interval" => $request["Interval"] ?? null,
+            "Period" => $request["Period"] ?? null,
+        ]));
+    }
+
+    // Метод отмены подписки на рекуррентные платежи.
+    // https://developers.cloudpayments.ru/#otmena-podpiski-na-rekurrentnye-platezhi
+    public function cancelSubscription(string $subscriptionId)
+    {
+        return $this->response($this->call("/subscriptions/cancel", [
+            "Id" => $subscriptionId
+        ]));
+    }
+
+    private function call(string $uri, array $data = null)
+    {
+        return $this->http->post("{$this->baseURL}{$uri}", $data);
+    }
+
+    private function response(Response $response)
     {
         $data = $response->throw()->json();
 
@@ -52,6 +157,6 @@ class CloudPaymentsService
             throw new Exception($data["Message"] ?? "Exception in CloudPayments Service");
         }
 
-        return $data["Model"];
+        return $data["Model"] ?? true;
     }
 }
